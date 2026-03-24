686ad234349f53761805537c0e05fd82c151bdeb
vimajeu <vitalina.hatsura.25@kse.org.ua>
2026-03-24 18:54:55 +0200

    part 2

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,23 @@
+namespace Week8;
+
+public class CargoContainer<T> where T : DeliveryItem
+{
+    private List<T> items = new List<T>();
+
+    public void AddItem(T item)
+    {
+        items.Add(item);
+        Console.WriteLine($"Added: {item.TrackingNumber}. Type: {typeof(T).Name}");
+    }
+
+    public double GetTotalCost()
+    {
+        double totalCost = 0;
+        foreach (var item in items)
+        {
+            totalCost += item.CalculateCost();
+        }
+
+        return totalCost;
+    }
+}
